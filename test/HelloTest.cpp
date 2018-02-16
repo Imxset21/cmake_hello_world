48c3a6ff4b8d52c1e18e02cb373a45606f96dd50
Pedro Rittner <pedro.rittner@posteo.net>
2018-02-15 22:27:30 -0800

    Use gtest for test case

--- test/HelloTest.cpp
+++ test/HelloTest.cpp
@@ -1,16 +1,23 @@
 #include <Hello.h>
 
 #include <iostream>
+#include <string>
 
 #include <gflags/gflags.h>
 #include <glog/logging.h>
+#include <gtest/gtest.h>
+
+TEST(TestHello, TestQueueConfig) {
+    ASSERT_EQ(*whirlpool::get_queue_config(), std::string{"ayy lmao"});
+}
 
 DEFINE_bool(verbose, false, "Whether to print extra debugging information");
 
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
-  
-  LOG(ERROR) << *whirlpool::get_queue_config();
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
 }
  
